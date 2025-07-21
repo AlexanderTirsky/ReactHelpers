a868219a78ee3f117e9c27c46c2979e31cf48234
Alexander_Tirsky <tirsky06@gmail.com>
2025-07-21 11:52:56 +1000

    added loader

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,19 @@ import cls from "./HomePage.module.css"
 import { API_URL } from "../../constants"
 import { useState, useEffect } from "react";
 import { QuestionCardList } from "../../components/QuestionCardList";
+import { Loader } from "../../components/Loader";
+import { delayFn } from "../../helpers/delayFn";
 
 
 export const HomePage = () => {
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+
+
   const getQuestions = async () => {
   try {
+    setIsLoading(true);
+    await delayFn()
     const response = await fetch(`${API_URL}/react`);
     const questions = await response.json();
 
@@ -16,6 +23,8 @@ export const HomePage = () => {
     console.log("questions", questions);
   } catch (error) {
     console.error(error);
+  } finally {
+    setIsLoading(false)
   }
 };
   useEffect(() => {
@@ -23,6 +32,7 @@ export const HomePage = () => {
   }, []);
   return ( 
   <>
+  {isLoading && <Loader />}
   <QuestionCardList cards={questions} />
   </>
   );
