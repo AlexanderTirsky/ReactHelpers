05d33511c6e25da984368da6c59ca816868ead83
Alexander_Tirsky <tirsky06@gmail.com>
2026-01-27 17:59:10 +0700

    date format helper

--- src/components/EditQuestionPage/EditQuestion.jsx
+++ src/components/EditQuestionPage/EditQuestion.jsx
@@ -2,9 +2,49 @@ import { useActionState } from "react";
 import cls from "./EditQuestionPage.module.css";
 import { Loader } from "../Loader";
 import { QuestionForm } from "../../components/QuestionForm";
+import { delayFn } from "../../helpers/delayFn";
+import { API_URL } from "../../constants";
+import { toast } from "react-toastify";
+import { dataFormat } from "../../helpers/dataFormat";
+
+const editCardAction = async (_prevState, formData) => {
+  try {
+    await delayFn();
+
+    const newQuestion = Object.fromEntries(formData);
+    const resources = newQuestion.resources.trim();
+    const isClearForm = newQuestion.clearForm;
+    const questionId = newQuestion.questionId;
+
+    const response = await fetch(`${API_URL}/react/${questionId}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        question: newQuestion.question,
+        answer: newQuestion.answer,
+        description: newQuestion.description,
+        resources: resources.length ? resources.split(",") : [],
+        level: Number(newQuestion.level),
+        completed: false,
+        editDate: dataFormat(new Date()),
+      }),
+    });
+
+    if (response.status === 404) {
+      throw new Error(response.statusText);
+    }
+
+    const question = response.json();
+    toast.success("Вопрос успешно отредактирован!");
+
+    return isClearForm ? {} : question;
+  } catch (error) {
+    toast.error(error.message);
+    return {};
+  }
+};
 
 export const EditQuestion = ({ initialState = {} }) => {
-  const [formState, formAction, isPending] = useActionState(() => {}, {
+  const [formState, formAction, isPending] = useActionState(editCardAction, {
     ...initialState,
     clearForm: false,
   });
